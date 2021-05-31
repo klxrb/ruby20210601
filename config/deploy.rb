6f2b21ead31e64454d9655612142a0869eb7ec00
parasquid <tristan.gomez@gmail.com>
2021-06-01 02:41:38 +0800

    add extra deployment settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,10 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "main"
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/app"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,10 +22,10 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +34,16 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+namespace :setup do
+  desc "copy config/master.key to shared/config"
+  task :copy_linked_master_key do
+    on roles :app do
+      upload! "config/master.key", "#{shared_path}/config/master.key"
+    end
+  end
+end
